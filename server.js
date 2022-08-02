c17a0a7e7db59b3ff99e8937aa2a33f8604e97cb
Chris <chrik119@gmail.com>
2022-08-02 12:54:51 -0700

    Twitch Connection Established

---
+++ server.js
@@ -0,0 +1,27 @@
+import tmiJs from 'tmi.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const regexpCommand = new RegExp(/^!([a-z0-9]+)(?:W+)?(.*)?/);
+
+const client = new tmiJs.Client({
+  options: { debug: true },
+  identity: {
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_OAUTH_TOKEN,
+  },
+  channels: ['zukshin'],
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+  if (self) return;
+
+  const [raw, command, args] = message.toLowerCase().match(regexpCommand);
+
+  if (command === 'hello') {
+    client.say(channel, `@${tags.username}, heya!`);
+  }
+});
